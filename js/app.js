72ac6acdb59969ebf33ceb329a43996a1a9458cd
Marte <marte.mk@hotmail.com>
2022-11-22 17:12:44 +0100

    Fixed an issue of missing button id and buttons causing issue with navigation

--- js/app.js
+++ js/app.js
@@ -6,23 +6,29 @@ const mobileOpenNav = document.querySelector("#mobile-hamburger");
 const mobileCloseNav = document.querySelector("#mobile-close");
 const nav = document.querySelector("#primary-navigation-wrapper");
 
-mobileOpenNav.addEventListener("click", (e) => {
-  openNav(mobileOpenNav, mobileCloseNav, nav);
-});
+if (mobileOpenNav) {
+  mobileOpenNav.addEventListener("click", (e) => {
+    openNav(mobileOpenNav, mobileCloseNav, nav);
+  });
+}
 
-mobileCloseNav.addEventListener("click", (e) => {
-  closeNav(mobileCloseNav, mobileOpenNav, nav);
-});
-
-document.addEventListener("click", (e) => {
-  if (
-    nav.getAttribute("data-visible") === "true" &&
-    e.target.closest("nav") !== nav &&
-    e.target.closest("button") !== mobileOpenNav
-  ) {
+if (mobileCloseNav) {
+  mobileCloseNav.addEventListener("click", (e) => {
     closeNav(mobileCloseNav, mobileOpenNav, nav);
-  }
-});
+  });
+}
+
+if (nav) {
+  document.addEventListener("click", (e) => {
+    if (
+      nav.getAttribute("data-visible") === "true" &&
+      e.target.closest("nav") !== nav &&
+      e.target.closest("button") !== mobileOpenNav
+    ) {
+      closeNav(mobileCloseNav, mobileOpenNav, nav);
+    }
+  });
+}
 
 // Theme updates
 const themeButton = document.querySelector("#theme-toggle");
